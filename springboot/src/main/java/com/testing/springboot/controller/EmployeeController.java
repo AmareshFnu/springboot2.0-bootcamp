026363197867f880f2c4867b2fdaccc9d1383ec8
amaresh <amaresh.excel@gmail.com>
2018-06-06 12:37:56 +0530

    have written services to trigger jenkins build from remote and added jenkins clients to pom.xml

--- springboot/src/main/java/com/testing/springboot/controller/EmployeeController.java
+++ springboot/src/main/java/com/testing/springboot/controller/EmployeeController.java
@@ -1,6 +1,10 @@
 package com.testing.springboot.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.offbytwo.jenkins.JenkinsServer;
+//import com.offbytwo.jenkins.JenkinsServer;
 import com.testing.springboot.dao.Employee;
 import com.testing.springboot.service.EmployeeService;
 
@@ -33,6 +39,25 @@ public class EmployeeController {
 	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteEmployee/{empId}")
 	private String createEmployee(@PathVariable String empId) {
 		employeeService.deleteEmployee(empId);
-		return "EmpID::" 	+ empId +  "has been deleted successfuly";
+		return "EmpID::" + empId + "has been deleted successfuly";
 	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/build/{jobname}")
+	// private String triggerJenkinsBuild(@PathVariable String jobname, String
+	// username, String password)
+	private String triggerJenkinsBuild(@PathVariable String jobname) throws URISyntaxException, IOException {
+
+		JenkinsServer jenkins = new JenkinsServer(new URI("http://localhost:8080"), "admin",
+				"ac1f8dd677de41f7942633e3118778af");
+
+		boolean crumbFlag = true;
+
+		Map<String, com.offbytwo.jenkins.model.Job> jobs = jenkins.getJobs();
+
+		jobs.get(jobname).build(crumbFlag);
+
+		System.out.println(jobs);
+		return "Build has been triggered successfuly";
+	}
+
 }
